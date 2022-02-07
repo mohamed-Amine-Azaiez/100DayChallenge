96451c3a7269a6ee4cf7d657baff39ed19a55539
mohamed amine azaiez <amine_azaiez@yahoo.fr>
2022-02-07 20:13:17 +0100

    Day 1

---
+++ Day 1/bandName.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.\n")
+city = input("What's name of the city you rew up in?\n")
+pet= input("what's your pet's name? \n")
+
+print("Your band name could be " + city + " " + pet)
